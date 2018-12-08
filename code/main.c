a22d78f5fab84c2db9d198003613da3bbbdbfe09
Monlicht <44339213+Monlicht@users.noreply.github.com>
2018-12-09 01:52:49 +0800

    Initial commit

---
+++ code/main.c
@@ -0,0 +1,20 @@
+//==========================
+//程序名称：推箱子(控制台)
+//==========================
+
+#include"function.h"
+#include <Windows.h>
+#include <stdio.h>
+#pragma comment(lib, "winmm.lib")
+
+
+int main()
+{
+	
+
+	init();
+
+	mainloop();
+
+	return 0;
+}
